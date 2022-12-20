8bcaa153edf7fb09caf721984a39a9727876d9d8
Vigneshwaran Elangovan <vygneshwaran@gmail.com>
2022-12-20 11:28:23 +0530

    flutter view inside a compose

--- app/src/main/java/com/vickycodes/flutterview/MainActivity.kt
+++ app/src/main/java/com/vickycodes/flutterview/MainActivity.kt
@@ -14,10 +14,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat.startActivity
-import io.flutter.embedding.android.FlutterActivity;
 import com.vickycodes.flutterview.ui.theme.FlutterViewTheme
 import io.flutter.FlutterInjector
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterView
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.dart.DartExecutor
@@ -25,24 +24,21 @@ import io.flutter.embedding.engine.dart.DartExecutor
 class MainActivity : ComponentActivity() {
 
     private var flutterView: FlutterView? = null
+    private var flutterEngine : FlutterEngine? = null
 
     private var flutterViewEngine: FlutterViewEngine? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val engine = FlutterEngine(applicationContext)
-        engine.dartExecutor.executeDartEntrypoint(
+        val args = getArgsFromIntent(intent)
+        flutterEngine = FlutterEngine(applicationContext, args)
+        flutterEngine?.dartExecutor?.executeDartEntrypoint(
             DartExecutor.DartEntrypoint(
                 FlutterInjector.instance().flutterLoader().findAppBundlePath(),
                 "main"))
 
-        flutterViewEngine = FlutterViewEngine(engine)
-        // The activity and FlutterView have different lifecycles.
-        // Attach the activity right away but only start rendering when the
-        // view is also scrolled into the screen.
-        flutterViewEngine?.attachToActivity(this)
 
         flutterView = FlutterView(this)
+        flutterView?.attachToFlutterEngine(flutterEngine!!)
         setContent {
             FlutterViewTheme {
                 // A surface container using the 'background' color from the theme
@@ -54,31 +50,50 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private fun getArgsFromIntent(intent: Intent): Array<String>? {
+        // Before adding more entries to this list, consider that arbitrary
+        // Android applications can generate intents with extra data and that
+        // there are many security-sensitive args in the binary.
+        val args = ArrayList<String>()
+        if (intent.getBooleanExtra("trace-startup", false)) {
+            args.add("--trace-startup")
+        }
+        if (intent.getBooleanExtra("start-paused", false)) {
+            args.add("--start-paused")
+        }
+        if (intent.getBooleanExtra("enable-dart-profiling", false)) {
+            args.add("--enable-dart-profiling")
+        }
+        if (!args.isEmpty()) {
+            return args.toTypedArray()
+        }
+        return null
+    }
+
+
+
+    /// hire will be tested if the channel lifecycle is resumed
+    override fun onResume() {
+        super.onResume()
+        flutterEngine!!.lifecycleChannel.appIsResumed()
+    }
+
+    // hire will be tested if the channel lifecycle is paused
+    override fun onPause() {
+        super.onPause()
+        flutterEngine!!.lifecycleChannel.appIsInactive()
+    }
+
+    // hire will be tested if the channel lifecycle is stoped
+    override fun onStop() {
+        super.onStop()
+        flutterEngine!!.lifecycleChannel.appIsPaused()
+    }
+
+    // hire will be tested if the channel lifecycle is destroied
     override fun onDestroy() {
+        flutterView!!.detachFromFlutterEngine()
         super.onDestroy()
-        flutterViewEngine?.detachActivity()
-    }
-
-    // These below aren't used here in this demo but would be needed for Flutter plugins that may
-    // consume these events.
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        flutterViewEngine?.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        flutterViewEngine?.onActivityResult(requestCode, resultCode, data)
-        super.onActivityResult(requestCode, resultCode, data)
-    }
-
-    override fun onUserLeaveHint() {
-        flutterViewEngine?.onUserLeaveHint()
-        super.onUserLeaveHint()
     }
 }
 
